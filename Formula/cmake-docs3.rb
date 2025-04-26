4c41ff6ddc4dc2629fa521e1d74442d143f3b935
Anton Melnikov <antonsm21@gmail.com>
2025-04-26 00:04:21 +0000

    cmake-docs3: add 3.31.7 bottle.

--- Formula/cmake-docs3.rb
+++ Formula/cmake-docs3.rb
@@ -5,6 +5,13 @@ class CmakeDocs3 < Formula
   sha256 "a6d2eb1ebeb99130dfe63ef5a340c3fdb11431cce3d7ca148524c125924cea68"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/botantony/homebrew-cmake3/releases/download/cmake-docs3-3.31.7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "05ab5e6fe32c5fa05061a52302f69f5b5c49a5c13b7acb28522130cc7da4b571"
+    sha256 cellar: :any_skip_relocation, ventura:       "2974482259c1f9fabc02f653adf8ddf4ca80fbabc0f0862a6f8ed95c75f8a48b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "edfe2803b7f20feabc34e825a1cce3e710c2bc9ce19da9eb732a9b47e2fdc21d"
+  end
+
   keg_only "conflicts with core formula"
 
   depends_on "cmake3" => :build
