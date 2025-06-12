546100f3968271fe455ae76d34e044e43f5cdeba
Anton Melnikov <antonsm21@gmail.com>
2025-06-12 22:47:28 +0000

    cmake-docs3: update 3.31.8 bottle.

--- Formula/cmake-docs3.rb
+++ Formula/cmake-docs3.rb
@@ -10,11 +10,11 @@ class CmakeDocs3 < Formula
   end
 
   bottle do
-    root_url "https://github.com/botantony/homebrew-cmake3/releases/download/cmake-docs3-3.31.7_1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "454e9cc1953ad1a1a10de3bc3e262060a0187ec0a1e6389cb26ce5c891f0dda8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6dbb66afebdd153bfdfab1385de3c59a4da7aeb33b16285a4cc99e1019d4abc4"
-    sha256 cellar: :any_skip_relocation, ventura:       "493ff02336f7b51090d9545f88a786d393161ed9d137051beea877c3b02c5585"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "44abded304bdcc37dd30e2315e93cfc273e1a4912412aaa1531b78313f0b6e26"
+    root_url "https://github.com/botantony/homebrew-cmake3/releases/download/cmake-docs3-3.31.8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9fac8539a3f8f4c279b49a3da88f2678e7dff8a6f497b4240df35f47916989c1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bcda41fa737103927b45913ad517a14a806f29925d6a56fd2dc74cbc6bf814ed"
+    sha256 cellar: :any_skip_relocation, ventura:       "d9ef56145676bbfc334cc1a68f0218c1e3015c2b186c745835133ea03ef83970"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ee140324803faadffddd6ac1c045e80554c2aa4989a5c21f35446abe9b311ae"
   end
 
   keg_only "conflicts with core formula"
