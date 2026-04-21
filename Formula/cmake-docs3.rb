075e7e8d9f3fbf081cb54516d659a02eb5ea3104
botantony <antonsm21@gmail.com>
2026-04-21 13:05:07 +0200

    cmake-docs3 3.31.21
    Closes #13.
    
    Signed-off-by: botantony <antonsm21@gmail.com>
    Signed-off-by: Anton Melnikov <antonsm21@gmail.com>

--- Formula/cmake-docs3.rb
+++ Formula/cmake-docs3.rb
@@ -1,8 +1,8 @@
 class CmakeDocs3 < Formula
   desc "Documentation for CMake"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.31.11/cmake-3.31.11.tar.gz"
-  sha256 "c0a3b3f2912b2166f522d5010ffb6029d8454ee635f5ad7a3247e0be7f9a15c9"
+  url "https://github.com/Kitware/CMake/releases/download/v3.31.12/cmake-3.31.12.tar.gz"
+  sha256 "5f3fd5a54dfa65602bdbed64f981a72673cc19f2d304cc2955cf0dfa0cfd8272"
   license "BSD-3-Clause"
 
   livecheck do
